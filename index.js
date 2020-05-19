49eb9347224e5326a9850903e4ca3eeaf2163bae
Lanre <l.olanipekun@yahoo.com>
2020-05-19 23:32:55 +0100

    release 1.0.1

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const firebase = require("firebase-admin");
+var admin = require("firebase-admin");
+var serviceAccount = require("./notification-e6ff4-firebase-adminsdk-g8kgu-4f1f1408b8.json");
+var nodemailer = require('nodemailer');
+var nodemailMailgun = require('nodemailer-mailgun-transport');
+
+firebase.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://notification-e6ff4.firebaseio.com"
+});
+
+module.exports = {
+    sendEmail: (html, mailGunApiKey, mailGunDomain, to, from, subject) => {
+        return new Promise((resolve, reject) => {
+            const auth = {
+                auth: {
+                    api_key: mailGunApiKey,
+                    domain: mailGunDomain
+                }
+            }
+            let transporter = nodemailer.createTransport(nodemailMailgun(auth));
+            const mailOptions = {
+                from,
+                to,
+                subject,
+                html
+            }
+            transporter.sendMail(mailOptions, (err, data) => {
+                if (err) {
+                    return reject(err)
+                }
+                resolve(data)
+            })
+        })
+    },
+    sendPushnotification: (firebaseToken, payload, options) => {
+        return new Promise((resolve, reject) => {
+            firebase.messaging().sendToDevice(firebaseToken, payload, options)
+                .then(res => {
+                    resolve(res)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+}
